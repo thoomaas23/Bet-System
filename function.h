e584011c00e114c7b56f1356a1af4c5542c73f21
thoomaas23 <thomas.lazar02@e-uvt.ro>
2023-02-26 17:03:06 -0800

    Username and Password

--- function.h
+++ function.h
@@ -4,12 +4,14 @@ void start()
     printf("Hi mate welcome in offline bet system  ! \n");
 }
 
-int files ()
+int login_panel ()
 {   FILE *fp;
  char filename[]= "username_and_password";
  char username[101];
  char password[101]; 
+ printf ("Put a username : ");
   gets(username);
+  printf ("Put a password : ");
   gets(password);
    fp = fopen(filename,"w+");
 
