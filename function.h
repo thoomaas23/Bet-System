f0e67cad9cf3e644ce1c12e6989875fbc1501428
thoomaas23 <thomas.lazar02@e-uvt.ro>
2023-02-26 18:00:52 -0800

    Login panel and start

--- function.h
+++ function.h
@@ -1,10 +1,18 @@
-void start()
-{
 
+bool start()
+{
+    char answear[5]; 
     printf("Hi mate welcome in offline bet system  ! \n");
+    printf("You are new here ? \n");
+    gets(answear); 
+    if (strcmp(answear ,  "Yes") == 0 )
+     return 1 ;
+     else 
+     return 0 ;
+
 }
 
-int login_panel ()
+void login_panel ()
 {   FILE *fp;
  char filename[]= "username_and_password";
  char username[101];
