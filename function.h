f34e32fb8bc2ae9e459389403a54934dad02e987
thoomaas23 <thomas.lazar02@e-uvt.ro>
2023-02-26 18:49:48 -0800

    Yea

--- function.h
+++ function.h
@@ -1,38 +1,34 @@
 
 bool start()
 {
-    char answear[5]; 
-    printf("Hi mate welcome in offline bet system  ! \n");
-    printf("You are new here ? \n");
-    gets(answear); 
-    if (strcmp(answear ,  "Yes") == 0 )
-     return 1 ;
-     else 
-     return 0 ;
-
+   char answear[5];
+   printf("Hi mate welcome in offline bet system  ! \n");
+   printf("You are new here ? \n");
+   gets(answear);
+   if (strcmp(answear, "Yes") == 0)
+      return 1;
+   else
+      return 0;
 }
 
-void login_panel ()
-{   FILE *fp;
- char filename[]= "username_and_password";
- char username[101];
- char password[101]; 
- printf ("Put a username : ");
-  gets(username);
-  printf ("Put a password : ");
-  gets(password);
-   fp = fopen(filename,"w+");
-
-   if ( fp )
-   {   fputs("Username : ", fp ); 
-	   fputs(username,fp);
-        fputs ("\n" , fp); 
-        fputs("Password : " , fp);
-       fputs(password,fp) ;
-    }
-   else
-      {
-         printf("Failed to open the file\n");
-        }
+void login_panel()
+{
+   FILE *fp;
+   char filename[] = "username_and_password";
+   char username[101];
+   char password[101];
+   printf("Put a username : ");
+   gets(username);
+   printf("Put a password : ");
+   gets(password);
+   fp = fopen(filename, "w+");
+   if (fp)
+   {
+      fputs("Username : ", fp);
+      fputs(username, fp);
+      fputs("\n", fp);
+      fputs("Password : ", fp);
+      fputs(password, fp);
+   }
    fclose(fp);
 }
