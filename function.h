1452b7be035261f609f5c11b365de4de226ba857
thoomaas23 <thomas.lazar02@e-uvt.ro>
2023-02-26 16:34:17 -0800

    New login panel

--- function.h
+++ function.h
@@ -1,6 +1,28 @@
-void start ()
-{ 
- 
-printf ("Hi mate welcome in offline bet system \n");
+void start()
+{
 
-}    
+    printf("Hi mate welcome in offline bet system  ! \n");
+}
+
+int files ()
+{   FILE *fp;
+ char filename[]= "username_and_password";
+ char username[101];
+ char password[101]; 
+  gets(username);
+  gets(password);
+   fp = fopen(filename,"w+");
+
+   if ( fp )
+   {   fputs("Username : ", fp ); 
+	   fputs(username,fp);
+        fputs ("\n" , fp); 
+        fputs("Password : " , fp);
+       fputs(password,fp) ;
+    }
+   else
+      {
+         printf("Failed to open the file\n");
+        }
+   fclose(fp);
+}
